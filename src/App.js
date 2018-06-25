b19de8a141dd9741e3596954de78d607ac42a37e
Yue Shu <yueshu7939@hotmail.com>
2018-06-25 15:34:53 -0400

    api

--- src/App.js
+++ src/App.js
@@ -9,48 +9,43 @@ const API_KEY = "40eb05a98ae9bdbfceccbc8109686408";
 
 
 class App extends React.Component {
-	state = {
-		temperature: undefined,
-		city: undefined,
-		country: undefined, 
-		humidity: undefined,
-		description: undefined,
-		error: undefined
-	}
-	getWeather = async (e) => {
-		e.preventDefault();
-		const city = e.target.elements.city.value;
-		const country = e.target.elements.country.value;
-		const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${API_KEY}&units=metric`); 
-		const data = await api_call.json();
-		if (city && country) {
-			console.log(data);
-			this.setState({
-				temperature: data.main.temp,
-				city: data.name,
-				country: data.sys.country,
-				humidity: data.main.humidity,
-				description: data.weather[0].description,
-				error: ''
-			});	
-		}
+        state = {
+            temperature: undefined,
+            city: undefined,
+            country: undefined,
+            humidity: undefined,
+            description: undefined,
+            error: undefined
+        }
+        getWeather = async(e) => {
+            e.preventDefault();
+            const city = e.target.elements.city.value;
+            const country = e.target.elements.country.value;
+            const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${API_KEY}&units=metric`);
+            const data = await api_call.json();
+            if (city && country) {
+                console.log(data);
+                this.setState({
+                    temperature: data.main.temp,
+                    city: data.name,
+                    country: data.sys.country,
+                    humidity: data.main.humidity,
+                    description: data.weather[0].description,
+                    error: ''
+                });
+            } else {
+                this.setState({
+                    temperature: undefined,
+                    city: undefined,
+                    country: undefined,
+                    humidity: undefined,
+                    description: undefined,
+                    error: "Please enter the city and country."
+                })
 
-		else {
-			this.setState({
-				temperature: undefined,
-				city: undefined,
-				country: undefined, 
-				humidity: undefined,
-				description: undefined,
-				error: "Please enter the city and country."
-			})
-			
+            }
 
-		}
-		
-
-
-	}
+        }
 	render() {
     return (
       <div>
